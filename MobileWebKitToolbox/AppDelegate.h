54cba9506485472731728d6b69024f296f2d21ea
Justin D'Arcangelo <justindarc@gmail.com>
2012-04-24 15:41:33 -0400

    Initial Commit

---
+++ MobileWebKitToolbox/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MobileWebKitToolbox
+//
+//  Created by Justin D'Arcangelo on 4/24/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ MobileWebKitToolbox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileWebKitToolbox
+//
+//  Created by Justin D'Arcangelo on 4/24/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
